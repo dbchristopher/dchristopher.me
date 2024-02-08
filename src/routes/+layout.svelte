56bb73ea57829d802f872a6cae9a69a88b495655
Daniel Christopher <dbchristopher@gmail.com>
2024-02-08 14:56:12 -0800

    basic site structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import '../reset.css'
+</script>
+
+<h1>Daniel Christopher</h1>
+
+<h2>Senior Frontend Software Engineer</h2>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/notes">Notes</a>
+  <a href="/about">About</a>
+  <a href="/contact">Contact</a>
+</nav>
+
+<slot></slot>
