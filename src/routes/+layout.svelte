e840b200ed80494403f79b1a02dfa2a3e55f55fc
Daniel Christopher <dbchristopher@gmail.com>
2024-02-09 11:51:56 -0800

    font style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
-  import '../reset.css'
+	import '@fontsource/fira-sans'; // Defaults to weight 400
+	import '@fontsource/fira-sans/400.css'; // Specify weight
+	import '@fontsource/fira-sans/700.css'; // Specify weight
+  import '@fontsource/fira-sans/400-italic.css'; // Specify
+  import '@fontsource/fira-sans/700-italic.css'; // Specify
+	import '../reset.css';
+  import '../app.css'
 </script>
 
 <h1>Daniel Christopher</h1>
@@ -7,10 +13,10 @@
 <h2>Senior Frontend Software Engineer</h2>
 
 <nav>
-  <a href="/">Home</a>
-  <a href="/notes">Notes</a>
-  <a href="/about">About</a>
-  <a href="/contact">Contact</a>
+	<a href="/">Home</a>
+	<a href="/notes">Notes</a>
+	<a href="/about">About</a>
+	<a href="/contact">Contact</a>
 </nav>
 
-<slot></slot>
+<slot />
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,39 +1,41 @@
-<style>
-  .hidden {
-    display: none;
-  }
-</style>
-
 <script>
-  export const prerender = true;
+	export const prerender = true;
 </script>
 
 <h1>Contact</h1>
 
 <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
-  <input type="hidden" name="form-name" value="contact">
-  <p class="hidden">
-    <label>
-      Don’t fill this out if you’re human: <input name="bot-field" />
-    </label>
-  </p>
+	<input type="hidden" name="form-name" value="contact" />
+	<p class="hidden">
+		<label>
+			Don’t fill this out if you’re human: <input name="bot-field" />
+		</label>
+	</p>
 
-  <p>
-    <label>Your Name: <input type="text" name="name" required /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" required /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple required>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
+	<p>
+		<label>Your Name: <input type="text" name="name" required /></label>
+	</p>
+	<p>
+		<label>Your Email: <input type="email" name="email" required /></label>
+	</p>
+	<p>
+		<label
+			>Your Role: <select name="role[]" multiple required>
+				<option value="leader">Leader</option>
+				<option value="follower">Follower</option>
+			</select></label
+		>
+	</p>
+	<p>
+		<label>Message: <textarea name="message"></textarea></label>
+	</p>
+	<p>
+		<button type="submit">Send</button>
+	</p>
 </form>
+
+<style>
+	.hidden {
+		display: none;
+	}
+</style>
