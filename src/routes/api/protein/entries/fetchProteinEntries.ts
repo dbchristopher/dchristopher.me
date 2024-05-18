d9c3b6737dc8c99da0632a822380798b60b90931
Daniel Christopher <dbchristopher@gmail.com>
2024-05-18 13:26:20 -0700

    standardize timestamps for entries

--- src/routes/api/protein/entries/fetchProteinEntries.ts
+++ src/routes/api/protein/entries/fetchProteinEntries.ts
@@ -1,11 +1,22 @@
 import { protein } from '$db/protein';
 
 export async function fetchProteinEntries(date: Date) {
-	const startOfDay = new Date(date.getTime());
-	startOfDay.setHours(0, 0, 0, 0);
+	// Determine if daylight saving time is in effect
+	const isDST = new Date().getTimezoneOffset() < 480;
 
-	const endOfDay = new Date(date.getTime());
-	endOfDay.setHours(23, 59, 59, 999);
+	// Pacific Time offset (7 hours for DST, 8 hours otherwise)
+	const pacificTimeOffset = isDST ? 7 : 8;
+
+	// Adjust the date to Pacific Time
+	date.setHours(date.getHours() - pacificTimeOffset);
+
+	const startOfDay = new Date(
+		Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0)
+	);
+
+	const endOfDay = new Date(
+		Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 23, 59, 59)
+	);
 
 	const entries = await protein
 		.find({
--- src/routes/api/protein/insertProteinEntry.ts
+++ src/routes/api/protein/insertProteinEntry.ts
@@ -6,10 +6,32 @@ export async function insertProteinEntry(
 	amount: number,
 	description: string
 ): Promise<InsertAuthTokenResponse> {
+	// Determine if daylight saving time is in effect
+	const isDST = new Date().getTimezoneOffset() < 480;
+
+	// Pacific Time offset (7 hours for DST, 8 hours otherwise)
+	const pacificTimeOffset = isDST ? 7 : 8;
+
+	const now = new Date();
+
+	// Adjust the date to Pacific Time
+	now.setHours(now.getHours() - pacificTimeOffset);
+
+	const createdTimestamp = new Date(
+		Date.UTC(
+			now.getUTCFullYear(),
+			now.getUTCMonth(),
+			now.getUTCDate(),
+			now.getUTCHours(),
+			now.getUTCMinutes(),
+			now.getUTCSeconds()
+		)
+	);
+
 	try {
 		// Insert the value into the specified collection
 		const collection = db.collection('protein');
-		await collection.insertOne({ amount, created: new Date(), description });
+		await collection.insertOne({ amount, created: createdTimestamp, description });
 		return { success: true };
 	} catch (error) {
 		return { success: false, error: error as Error };
