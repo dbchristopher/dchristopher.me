9ed5f875694c6c3df4334d289e463ab43a9a3f2c
Daniel Christopher <dbchristopher@gmail.com>
2024-07-17 14:30:24 -0700

    markdown parsing for notes page

--- src/routes/notes/[slug]/+page.server.ts
+++ src/routes/notes/[slug]/+page.server.ts
@@ -4,13 +4,12 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ params }) => {
 	try {
 		const blogEntries = await blog
-			.find({ slug: params.slug }, { projection: { created: 0, _id: 0 } })
+			.find({ slug: params.slug }, { projection: { _id: 0 } })
 			.limit(1)
 			.toArray();
 
 		return { post: blogEntries[0] };
 	} catch (error) {
-		console.error('Error querying MongoDB:', error);
 		return { props: { customers: [] } };
 	}
 };
--- src/routes/notes/[slug]/+page.svelte
+++ src/routes/notes/[slug]/+page.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import {marked} from 'marked'
+	import { NoteStatus} from '$lib/constants'
 
 	export let data: PageData;
 
-	$: ({ post } = data);
+	$: ({ post, isUserAuthenticated } = data);
 </script>
 
-<section>
+<article>
+	<header>
 	<h1>{post?.title}</h1>
+	<div class="byline">
+		<address class="author">by Daniel Christopher</address>
+		<time pubdate="pubdate" datetime="{post?.created}">Posted on {post?.created}</time>
+	{#if isUserAuthenticated} 
+		<a href="/notes/edit/{post?.slug}">Edit Post</a>
+	{/if}
+	{#if post?.status === NoteStatus.DRAFT}{post.status}{/if}
 	<ul>
 		{#each post?.tags as tag}
 			<li>{tag}</li>
 		{/each}
 	</ul>
+</header>
 
-	<p>{post?.content}</p>
-</section>
+	<p>{@html marked.parse(post?.content)}</p>
+</article>
