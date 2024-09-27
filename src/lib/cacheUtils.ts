83d188685774091a13a38584b3263ae64a29fbfc
Daniel Christopher <dbchristopher@gmail.com>
2024-09-27 12:34:20 -0700

    add cache logging

--- src/lib/cacheUtils.ts
+++ src/lib/cacheUtils.ts
@@ -10,17 +10,20 @@ type CacheData = {
 // Function to invalidate the cache
 export async function invalidateCache({ platform, cacheKey }: CacheData) {
 	if (platform?.context?.durable) {
+    console.log('DELETE DURABLE CACHE KEY:', cacheKey);
 		await platform.context.durable.delete(cacheKey);
 	} else {
-		console.log('DELETE CACHE KEY:', cacheKey);
+		console.log('DELETE IN-MEMORY CACHE KEY:', cacheKey);
 		inMemoryCache.delete(cacheKey);
 	}
 }
 
 export async function updateCache({ platform, data, cacheKey }: CacheData) {
 	if (platform?.context?.durable) {
+    console.log('UPDATE DURABLE CACHE KEY:', cacheKey);
 		await platform.context.durable.set(cacheKey, data, { ttl: 600 });
 	} else {
+    console.log('UPDATE IN-MEMORY CACHE KEY:', cacheKey);
 		inMemoryCache.set(cacheKey, { data, expiry: Date.now() + 600 * 1000 });
 	}
 }
@@ -29,12 +32,13 @@ export async function getCache({ platform, cacheKey }: CacheData) {
 	let data;
 
 	if (platform?.context?.durable) {
+    console.log('DURABLE CACHE HIT:', cacheKey);
 		data = await platform.context.durable.get(cacheKey);
 	} else {
 		const cached = inMemoryCache.get(cacheKey);
 		if (cached && cached.expiry > Date.now()) {
 			data = cached.data;
-			console.log('In-memory cache hit!');
+      console.log('IN-MEMORY CACHE HIT:', cacheKey);
 		}
 	}
 
