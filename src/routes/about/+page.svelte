791b40020e2826e7843a41d321e915c68b5bc558
Daniel Christopher <dbchristopher@gmail.com>
2024-12-11 14:07:47 -0800

    about page update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,22 +10,12 @@
 			<h1>About</h1>
 		</header>
 		<div class="content">
+			<p>I'm a marginally online, non-influencer. Not a guru or a thought leader.</p>
 			<p>
-				I'm a software developer who brings ideas to life through code, a fitness enthusiast always
-				striving for personal growth, a writer exploring diverse topics, and a dedicated parent.
+				Professionaly, I'm a software developer, though I'll only occasionally write about that. I'm
+				just a guy trying to figure out life, and I wanted my own little slice of the internet to
+				share some things I'm thinking about.
 			</p>
-			<p>
-				I created this website as a throwback to a time when the internet was a little more DIY. I'm
-				tired of the consolidation of the internet into a few major social network players and
-				simply wanted my own custom-built playground. I may be intentionally isolating myself from
-				social networking opportunities, but it feels good to fully control my own work and content.
-			</p>
-			<p>This was crafted with the following technologies:</p>
-			<ul>
-				<li>SvelteKit</li>
-				<li>MongoDB</li>
-				<li>Netlify</li>
-			</ul>
 		</div>
 	</article>
 </ContentWrapper>
