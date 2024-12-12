61ce9961dc169e6786b775c589af824f19de3528
Daniel Christopher <dbchristopher@gmail.com>
2024-12-12 14:20:04 -0800

    faq update

--- src/routes/protein/Faq.svelte
+++ src/routes/protein/Faq.svelte
@@ -4,10 +4,10 @@
 <div>
 	<h3>What is this?</h3>
 	<p>
-		This is a simple list of all the protein consume, with the explicit goal of trying to eat 1 gram
-		of protein per pound of bodyweight. I created this simple application for fun and simply because
-		I got tired of managing a spreadsheet on my phone throughout the day. I don't expect you to be
-		all that interested, but I decided to allow this data to be public in case it inspires you to
+		This is a simple list of all the protein consume and a note about the sources. What gets
+		measured gets managed, and I have found that it's much easier to reach my protein target every
+		day if I write it down somewhere. This is not a complete dietary journal, as I'm only currently
+		tracking that single macro. I decided to allow this data to be public in case it inspires you to
 		also be more proactive in tracking your diet and fitness goals.
 	</p>
 </div>
