7fbf47068ec33263e62ec685287d39051a340831
Daniel B Christopher <hello@dchristopher.me>
2025-02-06 13:27:47 -0800

    use link navigation

--- src/routes/protein/[...slug]/DatePicker.svelte
+++ src/routes/protein/[...slug]/DatePicker.svelte
@@ -13,9 +13,11 @@
 
 <div class="date-picker">
 	<md-filled-button onclick={() => {}}>&#8592;</md-filled-button>
-	<md-filled-tonal-button kind="tertiary" onclick={() => {}}
-		>{date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}</md-filled-tonal-button
-	>
+	<a href="/protein/today" class="no-underline">
+		<md-filled-tonal-button kind="tertiary" >
+			{date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}
+		</md-filled-tonal-button>
+	</a>
 	<md-filled-button onclick={() => {}}>&#8594;</md-filled-button>
 </div>
 
