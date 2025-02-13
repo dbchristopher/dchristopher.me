88c1a4d9a6af1b3ead26d29102ff8ae59cd4532b
Daniel B Christopher <hello@dchristopher.me>
2025-02-13 14:56:12 -0800

    update date cache key

--- src/routes/api/protein/formatDateKeywordCacheKey.ts
+++ src/routes/api/protein/formatDateKeywordCacheKey.ts
@@ -3,5 +3,5 @@ import { PROTEIN_CACHE_KEY } from '$lib/constants';
 
 export function formatDateKeywordCacheKey(date: Date) {
 	const normalizedDate = normalizeDateTime(date);
-	return `${PROTEIN_CACHE_KEY}-${normalizedDate.toISOString()}`;
+	return `${PROTEIN_CACHE_KEY}-${normalizedDate.getUTCFullYear()}/${normalizedDate.getUTCMonth()}/${normalizedDate.getUTCDay()}`;
 }
